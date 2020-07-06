a3c6ff721a0ecf181e408123b585af612a4a01a9
Hua <54726816+hy2632@users.noreply.github.com>
2020-07-05 20:13:31 -0400

    有穷性

--- DataStructure/youqiongxing.java
+++ DataStructure/youqiongxing.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class youqiongxing{
     public static void main(String[] args){
-        int[] hs = Hailstone(165);
+        int[] hs = Hailstone(27);
         System.out.println(Arrays.toString(hs));
     }
 
@@ -12,9 +12,11 @@ public class youqiongxing{
             return hs;
         }
         else if(n % 2 ==0){
-            // 两数组合并
             int[] hs_right = Hailstone(n/2);
             int[] hs_left = {n};
+
+            // 两数组合并，比python列表直接相加复杂很多
+            // https://www.cnblogs.com/firstdream/p/4634834.html
             int[] hs = new int[hs_right.length+1];
             System.arraycopy(hs_left, 0, hs, 0, 1);
             System.arraycopy(hs_right, 0, hs, 1, hs_right.length);
@@ -29,3 +31,5 @@ public class youqiongxing{
         }
     }
 }
+
+// 数据结构01-A-2，有穷性。
